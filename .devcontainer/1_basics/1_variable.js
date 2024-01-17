1895e87b509d1afb71c1574aaa654c3deba0060a
Aminegijj <87919914+Aminegijj@users.noreply.github.com>
2024-01-17 04:26:27 +0000

    container config done

---
+++ .devcontainer/1_basics/1_variable.js
@@ -0,0 +1,13 @@
+ const accountId = 29102001
+let accountEmail= "Amitjj@gmail.com"
+var accountPassword = "11233"
+accountCity = "Doon"
+let accountState=""
+
+accountEmail= "abc@gmail.com"
+accountPassword="aabbss"
+accountCity- "delhi"
+
+console.log(accountId);
+
+console.table(accountId, accountEmail, accountPassword, accountCity, accountState)
